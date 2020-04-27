be3d49dc3490e93b14d5f8d8794f229462a31e14
Caian R. Ertl <hi@caian.org>
2020-04-27 01:44:21 -0300

    feat: clone a single branch, checkout to specific commit

--- main.go
+++ main.go
@@ -7,17 +7,39 @@ import (
     "github.com/go-git/go-billy/v5/memfs"
     "github.com/go-git/go-git/v5"
     "github.com/go-git/go-git/v5/storage/memory"
+    "github.com/go-git/go-git/v5/plumbing"
 )
 
 func main() {
     fs := memfs.New()
     storer := memory.NewStorage()
 
-    _, err := git.Clone(storer, fs, &git.CloneOptions{
+    reference := plumbing.NewBranchReferenceName("master")
+
+    log.Println("clonning repository in memory")
+    repo, err := git.Clone(storer, fs, &git.CloneOptions{
         URL: "https://github.com/tiny-ci/example",
+        ReferenceName: reference,
+        SingleBranch: true,
+        Depth: 50,
+        Tags: git.NoTags,
     })
 
     if err != nil {
+        log.Fatal(err)
+    }
+
+    worktree, err := repo.Worktree()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = worktree.Checkout(&git.CheckoutOptions{
+        Hash: plumbing.NewHash("dba8a3250ff364a8a1ccfe0ca0b1bdeb43adadcb"),
+    })
+
+    if err != nil {
+        log.Println("cannot checkout")
         log.Fatal(err)
     }
 
