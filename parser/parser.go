9407ed562fb3288d67829deca521d42bf659bbf9
Caian R. Ertl <hi@caian.org>
2020-04-28 06:11:12 -0300

    feat: package to parse received data

---
+++ parser/parser.go
@@ -0,0 +1,17 @@
+package parser
+
+import (
+	"github.com/tiny-ci/core/types"
+	"gopkg.in/yaml.v2"
+)
+
+func ParsePipeConfig(content []byte) (*types.PipeConfig, error) {
+    config := types.PipeConfig{}
+
+    err := yaml.Unmarshal(content, &config)
+    if err != nil {
+        return nil, err
+    }
+
+    return &config, nil
+}
