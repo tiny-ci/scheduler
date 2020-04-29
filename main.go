4bc823ec646bdaefa4068f661d5bbf6a12595469
Caian R. Ertl <hi@caian.org>
2020-04-29 17:25:45 -0300

    feat: (WIP) implement logic to filter jobs by "when" conditionals

--- main.go
+++ main.go
@@ -2,17 +2,91 @@ package main
 
 import (
     "encoding/json"
+    "fmt"
     "log"
     "github.com/tiny-ci/core/parser"
     "github.com/tiny-ci/core/pipeconf"
     "github.com/tiny-ci/core/schema"
+    "github.com/tiny-ci/core/types"
 )
+
+func isStringSlice(intf interface{}) bool {
+    switch intf.(type) {
+    case []interface{}:
+        return true
+
+    default:
+        return false
+    }
+}
+
+func toStringSlice(intf interface{}) []string {
+    var ss []string
+
+    if isStringSlice(intf) {
+        for _, item := range intf.([]interface{}) {
+            ss = append(ss, fmt.Sprintf("%s", item))
+        }
+    } else {
+        ss = append(ss, fmt.Sprintf("%s", intf))
+    }
+
+    return ss
+}
+
+func appendJob(jobs *[]types.Job, pipeJob *types.PipeJob) {
+    *jobs = append(*jobs, types.Job{
+        Name:  pipeJob.Name,
+        Image: pipeJob.Image,
+        Steps: toStringSlice(pipeJob.Steps),
+    })
+}
+
+func filter(config *types.PipeConfig, ref string) []types.Job {
+    matchedJobs := []types.Job{}
+
+    for _, pipeJob := range config.Jobs {
+        if pipeJob.When == nil {
+            appendJob(&matchedJobs, &pipeJob)
+            continue
+        }
+
+        if pipeJob.When.Branch == nil && pipeJob.When.Tag == nil {
+            continue
+        }
+
+        var conditionals []string
+
+        if pipeJob.When.Branch != nil {
+            conditionals = append(conditionals, toStringSlice(pipeJob.When.Branch)...)
+        }
+
+        if pipeJob.When.Tag != nil {
+            conditionals = append(conditionals, toStringSlice(pipeJob.When.Tag)...)
+        }
+
+        hasMatch := false
+        for _, cond := range conditionals {
+            if ref == cond {
+                appendJob(&matchedJobs, &pipeJob)
+                hasMatch = true
+                break
+            }
+        }
+
+        if hasMatch { continue }
+    }
+
+    fmt.Println(matchedJobs)
+
+    return matchedJobs
+}
 
 func main() {
     configContent, err := pipeconf.Fetch(&pipeconf.GitRef{
         Name: "master",
         URL:  "https://github.com/tiny-ci/example",
-        Hash: "dba8a3250ff364a8a1ccfe0ca0b1bdeb43adadcb",
+        Hash: "961240b68021e7d8ecb53c0b5b4e1e8e097fb319",
         IsTag: false,
     })
 
@@ -39,5 +113,5 @@ func main() {
         log.Fatal(err)
     }
 
-    log.Println(config.Jobs[0].When.Tag)
+    filter(config, "master")
 }
