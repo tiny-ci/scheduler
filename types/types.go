bb6337c6d7ac33d24bb3f570c9d5a216d1a8786c
Caian R. Ertl <hi@caian.org>
2020-04-29 17:22:03 -0300

    feat: tag cond can be string or string slice; job struct

--- types/types.go
+++ types/types.go
@@ -1,17 +1,23 @@
 package types
 
-type PipeWhenCond struct {
+type PipeConditional struct {
     Branch interface{} `json:"branch,omitempty" yaml:"branch"`
-    Tag    string      `json:"tag,omitempty"    yaml:"tag"`
+    Tag    interface{} `json:"tag,omitempty"    yaml:"tag"`
 }
 
 type PipeJob struct {
-    Name  string       `json:"name"  yaml:"name"`
-    Image string       `json:"image" yaml:"image"`
-    Steps interface{}  `json:"steps" yaml:"steps"`
-    When *PipeWhenCond `json:"when"  yaml:"when"`
+    Name  string          `json:"name"  yaml:"name"`
+    Image string          `json:"image" yaml:"image"`
+    Steps interface{}     `json:"steps" yaml:"steps"`
+    When *PipeConditional `json:"when"  yaml:"when"`
 }
 
 type PipeConfig struct {
     Jobs []PipeJob `json:"jobs" yaml:"jobs"`
+}
+
+type Job struct {
+    Name  string   `json:"name"`
+    Image string   `json:"image"`
+    Steps []string `json:"steps"`
 }
