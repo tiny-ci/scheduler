bcf66eb3cea0001fcdc18250ba9bfc67538cd6e3
Caian R. Ertl <hi@caian.org>
2020-04-30 05:07:25 -0300

    feat: bunch of stuff that i'll eventually organize

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
     "encoding/json"
     "log"
+    "github.com/tiny-ci/core/db"
     "github.com/tiny-ci/core/parser"
     "github.com/tiny-ci/core/pipe"
     "github.com/tiny-ci/core/schema"
 )
 
 func main() {
+    ref := "master"
     configContent, err := pipe.Fetch(&pipe.GitRef{
-        Name: "master",
-        URL:  "https://github.com/tiny-ci/example",
-        Hash: "961240b68021e7d8ecb53c0b5b4e1e8e097fb319",
+        Name: ref,
+        URL:  "https://github.com/caiertl/tmp",
+        Hash: "64954853e0169c930024d24563883f9ea59a0c9a",
         IsTag: false,
     })
 
     if err != nil {
+        log.Println("fetch error")
         log.Fatal(err)
+    }
+
+    if err == nil && configContent == nil {
+        log.Println("pipe config not found")
+        return
     }
 
     config, err := parser.ParsePipeConfig(configContent.Bytes())
@@ -39,5 +47,18 @@ func main() {
         log.Fatal(err)
     }
 
-    matchedJobs := pipe.Filter(config, "master")
+    matchedJobs, err := pipe.Filter(config, ref)
+    if err != nil {
+        log.Println("filter error")
+        log.Fatal(err)
+    }
+
+    log.Println(matchedJobs)
+
+    rdb := db.RedisDatabase{}
+    err = rdb.Connect()
+    if err != nil {
+        log.Println("redis error")
+        log.Fatal(err)
+    }
 }
