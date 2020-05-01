f9ac68dc940a4a3165078f2efd129d3cad1620f8
Caian R. Ertl <hi@caian.org>
2020-05-01 11:33:05 -0300

    feat: New function, bulk insertion

--- db/redis.go
+++ db/redis.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+    "encoding/json"
+    "fmt"
+    "github.com/tiny-ci/core/types"
     "github.com/go-redis/redis/v7"
 )
 
@@ -8,11 +11,42 @@ type RedisDatabase struct {
     client *redis.Client
 }
 
-func (r RedisDatabase) Connect() error {
-    r.client = redis.NewClient(&redis.Options{
-        Addr: "localhost:6379",
-    })
+func New(addr string) (RedisDatabase, error) {
+    rdb := RedisDatabase{
+        client: redis.NewClient(&redis.Options{
+            Addr: addr,
+        }),
+    }
 
-    err := r.client.Ping().Err()
-    return err
+    return rdb, rdb.client.Ping().Err()
+}
+
+func (r RedisDatabase) NewGitInfoRepr(pipeId string, key string) string {
+    return fmt.Sprintf("pipe:%s:git:%s", pipeId, key)
+}
+
+func (r RedisDatabase) NewJobInfoRepr(pipeId string, key string, i int) string {
+    return fmt.Sprintf("pipe:%s:job:%d:%s", pipeId, i, key)
+}
+
+func (r RedisDatabase) Populate(ntf *types.ApiNotification, jobs *[]types.Job) error {
+    pi := ntf.PipelineId
+    item := make(map[string]interface{})
+
+    item[r.NewGitInfoRepr(pi, "repo_url")]    = ntf.Info.URL
+    item[r.NewGitInfoRepr(pi, "ref_name")]    = ntf.Info.RefName
+    item[r.NewGitInfoRepr(pi, "commit_hash")] = ntf.Info.CommitHash
+    item[r.NewGitInfoRepr(pi, "is_tag")]      = fmt.Sprint(ntf.Info.IsTag)
+
+    for i, job := range *jobs {
+        steps, err := json.Marshal(job.Steps)
+        if err != nil { return err }
+
+        item[r.NewJobInfoRepr(pi, "name", i)]  = job.Name
+        item[r.NewJobInfoRepr(pi, "image", i)] = job.Image
+        item[r.NewJobInfoRepr(pi, "steps", i)] = steps
+    }
+
+    res := r.client.MSet(item)
+    return res.Err()
 }
