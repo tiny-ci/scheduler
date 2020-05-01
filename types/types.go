b20831548a292723d8513c7b1dfebd40f4a0bc79
Caian R. Ertl <hi@caian.org>
2020-05-01 11:32:28 -0300

    feat: type to unmarshal the notification message

--- types/types.go
+++ types/types.go
@@ -21,3 +21,15 @@ type Job struct {
     Image string   `json:"image"`
     Steps []string `json:"steps"`
 }
+
+type GitInfo struct {
+    URL        string `json:"url"`
+    RefName    string `json:"ref_name"`
+    CommitHash string `json:"commit_hash"`
+    IsTag      bool   `json:"is_tag"`
+}
+
+type ApiNotification struct {
+    PipelineId string  `json:"pipeline_id"`
+    Info       GitInfo `json:"git"`
+}
