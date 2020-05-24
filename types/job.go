42d036327c59d559264d05746bdfe0a9f3ab57f0
Caian R. Ertl <hi@caian.org>
2020-05-24 06:49:54 -0300

    feat: "engine" field to job struct

--- types/job.go
+++ types/job.go
@@ -1,7 +1,8 @@
 package types
 
 type Job struct {
-    Name  string   `json:"name"`
-    Image string   `json:"image"`
-    Steps []string `json:"steps"`
+    Name   string   `json:"name"`
+    Engine string   `json:"engine"`
+    Image  string   `json:"image"`
+    Steps  []string `json:"steps"`
 }
