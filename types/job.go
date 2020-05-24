e62b9a154e55db26fbbb0d721a963bef9e8bfc8b
Caian R. Ertl <hi@caian.org>
2020-05-24 06:52:15 -0300

    fix: remove unused tags

--- types/job.go
+++ types/job.go
@@ -1,8 +1,8 @@
 package types
 
 type Job struct {
-    Name   string   `json:"name"`
-    Engine string   `json:"engine"`
-    Image  string   `json:"image"`
-    Steps  []string `json:"steps"`
+    Name   string
+    Engine string
+    Image  string
+    Steps  []string
 }
